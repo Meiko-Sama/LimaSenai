cdd552da02489a9c0f5d0606cc6f8c53ed5e941b
Aluno <thayna.matos.carlos@gmail.com>
2025-02-19 15:34:38 -0300

    TERMINEDO

--- HTMLeJS/math.js
+++ HTMLeJS/math.js
@@ -9,22 +9,44 @@ console.log(Math.round(4.3));
 console.log(Math.floor(4.9));
 console.log(Math.floor(4.7));
 
-// ARREDONDA PARA CIMA (CEIL)//
+// ARREDONDA PARA CIMA (CEIL) //
 
 console.log(Math.ceil(4.2));
 console.log(Math.ceil(4.1));
 
-// RETORNA SEMPRE O MAIOR VALOR //
+// RETORNA SEMPRE O MAIOR VALOR (MAX) //
 
 console.log(Math.max(10, 30, 40, 20));
 console.log(Math.max(100, 20, 30, 10));
 
-// RETORNA SEMPRE O MENOR VALOR //
+// RETORNA SEMPRE O MENOR VALOR (MIN) //
 
 console.log(Math.max(1, 30, 13, 86));
 console.log(Math.max(100, 71, 32, 10));
 
-// EXPONENCIAÇÃO (BASE ^ EXPOENTE) //
+// EXPONENCIAÇÃO (BASE ^ EXPOENTE) (POW) //
 
 console.log(Math.pow(2, 3));
 console.log(Math.pow(4, 8));
+
+// RETORNA A RAIZ QUADRADA (SQRT) //
+
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(4));
+
+// PI = Dirá o valor do PI, não precisa de () e ponto virgula //
+
+const pi = Math.PI;
+console.log(Math.PI);
+
+// GERA NUMERO ALEATORIO ENTRE 0 e 1 (RANDOM) //
+// GERA NUMERO ALEATORIO ENTRE 0 e 10 (RANDOM) //
+
+const qntNumeros = 10;
+console.log(Math.random() * (qntNumeros + 1));
+
+// GERA NUMERO ALEATORIO ENTRE MAX e MIN (Incluindo os dois) //
+
+const max = 10;
+const min = 1;
+console.log(Math.random() * (max - min + 1) + min);
---
+++ HTMLeJS/string.js
@@ -0,0 +1,43 @@
+let frase = " Uma frase qualquer! ";
+
+let palavras = ["Uma", "frase"];
+
+// TAMANHO DA STRING //
+console.log(frase.length);
+
+// CARACTERES EM MAIÚSCULOS //
+console.log(frase.toUpperCase());
+
+// CARACTERES EM MINÚSCULOS //
+console.log(frase.toLowerCase());
+
+// REMOVE OS ESPAÇO DO INICIO E DO FINAL DO STRING //
+console.log(frase.trim());
+
+// ENCONTRA A PRIMEIRA OCORRÊNCIA DA PALAVRA NA STRING  //
+console.log(frase.indexOf("frase"));
+
+// ENCONTRA A ULTIMA OCORRÊNCIA DA PALAVRA NA STRING  //
+console.log(frase.lastIndexOf("frase"));
+
+// VERIFICAR SE A PALAVRA CONTEM NA STRING OU NAO  //
+console.log(frase.includes("frase"));
+
+// EXTRAIR UMA PRATE DA STRING //
+console.log(frase.slice(3, 6));
+
+// SUBSTITUIR A TODAS AS OCORRÊNCIA  //
+console.log(frase.replaceAll("frase", "incrivel"));
+
+// JUNTAR OS ELEMENTOS DE ARRAY  //
+console.log(palavras.join(" "));
+
+// ESEPARAR UMA STRING EM UM ARRAY  //
+console.log(frase.split(" "));
+
+// INVERTER O MEU ARRAY DE STRING //
+console.log(frase.split(" ").reverse());
+console.log(palavras.reverse());
+
+// REMOVER TODOS OS ESPAÇOS//
+console.log(frase.replace(/\s+/g, ""));
